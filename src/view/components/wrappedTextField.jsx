3638b96682d55b7ec1eee3f8d547ee3b8b73c0b6
Serban Stancu <stancu@adobe.com>
2022-08-30 11:30:58 -0600

    Fix layout problems that appeared when the latest react-spectrum is used.

--- src/view/components/wrappedTextField.jsx
+++ src/view/components/wrappedTextField.jsx
@@ -3,7 +3,6 @@ Copyright 2020 Adobe. All rights reserved.
 This file is licensed to you under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License. You may obtain a copy
 of the License at http://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software distributed under
 the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
 OF ANY KIND, either express or implied. See the License for the specific language
@@ -54,6 +53,7 @@ export default function AdvancedSection({
   ...rest
 }) {
   const methods = useFormContext();
+  const hasLabel = Boolean(rest.label);
 
   return (
     <Controller
@@ -85,9 +85,8 @@ export default function AdvancedSection({
           {supportDataElement && (
             <ActionButton
               aria-label="Open data element selector"
-              alignSelf="flex-end"
+              marginTop={hasLabel ? 'size-300' : ''}
               marginStart="size-65"
-              marginBottom="size-225"
               onPress={openDataElementSelector(
                 supportDataElement,
                 name,
